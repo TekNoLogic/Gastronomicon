de9a0c1e5d79140d7341b0fd5b06eea5b58d06e0
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-10-22 02:24:02 -0600

    dead code

--- Gastronomicon.lua
+++ Gastronomicon.lua
@@ -5,49 +5,10 @@ local myname, ns = ...
 local HookedButtons = {} -- [button] = true
 local LocalizedIngredientList = {} -- [itemID] = {itemName, itemLink}
 local TooltipInfo = {} -- [button] = {[i] = 'recipe name?'}
-local RecipeCache = {} -- [recipeID] = info
 
 
 local f = CreateFrame('frame')
 
-local RegisteredFrames = {} -- Holds a list of frames that should be registered for TRADE_SKILL_SHOW after our addon is finished
-local Callback
-local function RequestCookingStuff(callback)
-	Callback = callback
-	if C_TradeSkillUI.GetTradeSkillLine() ~= 185 then
-		RegisteredFrames = {GetFramesRegisteredForEvent('TRADE_SKILL_SHOW')}
-		for _, frame in pairs(RegisteredFrames) do
-			frame:UnregisterEvent('TRADE_SKILL_SHOW')
-		end
-		f:RegisterEvent('TRADE_SKILL_SHOW')
-		-- There seems to be no other way to prevent the tradeskill ui from opening when we call this function,
-		-- so we have to make SURE that the event always get re-registered or we'll break the other tradeskills
-		local opened = C_TradeSkillUI.OpenTradeSkill(185)
-		if not opened then
-			f:UnregisterEvent('TRADE_SKILL_SHOW')
-			for _, frame in pairs(RegisteredFrames) do
-				frame:RegisterEvent('TRADE_SKILL_SHOW')
-			end
-		end
-	else
-		f:GetScript('OnEvent')(f, 'TRADE_SKILL_LIST_UPDATE')
-	end
-end
-
--- Cache relevent recipe info for lookups
-local function CacheRecipes()
-	wipe(RecipeCache)
-	for ingredientItemID, recipes in pairs(ns.recipes) do
-		for _, recipeID in pairs(recipes) do
-			RecipeCache[recipeID] = C_TradeSkillUI.GetRecipeInfo(recipeID)
-		end
-	end
-	for _, recipes in pairs(ns.requisites) do
-		for _, recipeID in pairs(recipes) do
-			RecipeCache[recipeID] = C_TradeSkillUI.GetRecipeInfo(recipeID)
-		end
-	end
-end
 
 local IsNomi = false -- Are we currently interacting with Nomi?
 local function DecorateNomi()
@@ -86,14 +47,12 @@ local function DecorateNomi()
 					local canLearn = 0
 					local ingredientName, ingredientLink = ingredient[1], ingredient[2]
 					for _, recipeID in pairs(recipeList) do
-						local info = RecipeCache[recipeID]
 						if ns.GetRecipeName(recipeID) and not ns.GetRecipeLearned(recipeID) then
 							unlearned = unlearned + 1
 							local learnable = true
 							if ns.requisites[recipeID] then
 								for _, requisiteID in pairs(ns.requisites[recipeID]) do
 									-- we must know all requisites to be able to receive this item
-									local requisiteInfo = RecipeCache[requisiteID] -- C_TradeSkillUI.GetRecipeInfo(requisiteID)
 									if ns.GetRecipeName(requisiteID) and not ns.GetRecipeLearned(requisiteID) then
 										-- we're missing one of the requisites, can't make this
 										learnable = false
@@ -154,19 +113,6 @@ f:SetScript('OnEvent', function(self, event, ...)
 		end
 	elseif event == 'GOSSIP_CLOSED' then
 		IsNomi = false
-	elseif event == 'TRADE_SKILL_SHOW' then
-		self:UnregisterEvent('TRADE_SKILL_SHOW')
-		self:RegisterEvent('TRADE_SKILL_LIST_UPDATE')
-		for _, frame in pairs(RegisteredFrames) do
-			frame:RegisterEvent('TRADE_SKILL_SHOW')
-		end
-	elseif event == 'TRADE_SKILL_LIST_UPDATE' then
-		self:UnregisterEvent('TRADE_SKILL_LIST_UPDATE')
-		if Callback then
-			CacheRecipes()
-			Callback()
-		end
-		C_TradeSkillUI.CloseTradeSkill()
 	elseif event == 'GET_ITEM_INFO_RECEIVED' then
 		local itemID = ...
 		if ns.recipes[itemID] then
