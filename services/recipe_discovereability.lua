a2df82b24525fbe57599dc32c60a8551a34c42e7
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-10-25 21:07:58 -0600

    Remove tier 3 prerequisites

--- services/recipe_discovereability.lua
+++ services/recipe_discovereability.lua
@@ -17,12 +17,10 @@ function ns.CanBeDiscovered(recipe_id)
 		if not ns.GetRecipeLearned(previous_id) then return false end
 	end
 
-	if not ns.requisites[recipe_id] then return true end
-
-	for _,requisite_id in pairs(ns.requisites[recipe_id]) do
-		if not ns.GetRecipeName(requisite_id) then return false end
-		if not ns.GetRecipeLearned(requisite_id) then return false end
-	end
+	local requisite_id = ns.requisites[recipe_id]
+	if not requisite_id then return true end
+	if not ns.GetRecipeName(requisite_id) then return false end
+	if not ns.GetRecipeLearned(requisite_id) then return false end
 
 	return true
 end
